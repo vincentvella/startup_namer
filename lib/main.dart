1b63be87bcd1a2602201fdef9c9fe5c8aef0a0bf
Vincent Vella <vincevella0@gmail.com>
2020-08-18 20:15:41 -0400

    Add swipe to refresh interaction

--- lib/main.dart
+++ lib/main.dart
@@ -72,18 +72,23 @@ class _RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildSuggestions() {
-    return ListView.builder(
-        padding: const EdgeInsets.all(16),
-        itemBuilder: (BuildContext _context, int i) {
-          if (i.isOdd) {
-            return Divider();
-          }
-          final int index = i ~/ 2;
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
-          return _buildRow(_suggestions[index]);
-        });
+    return RefreshIndicator(
+      child: ListView.builder(
+          padding: const EdgeInsets.all(16),
+          itemBuilder: (BuildContext _context, int i) {
+            if (i.isOdd) {
+              return Divider();
+            }
+            final int index = i ~/ 2;
+            if (index >= _suggestions.length) {
+              _suggestions.addAll(generateWordPairs().take(10));
+            }
+            return _buildRow(_suggestions[index]);
+          }),
+      onRefresh: () async {
+        return await Future.delayed(Duration(seconds: 3));
+      },
+    );
   }
 
   @override
